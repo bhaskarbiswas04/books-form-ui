beba19cc4527c347e5c7f903af1474917bec2239
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2025-10-24 09:07:12 +0530

    books delete route added

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,11 +1,38 @@
-import useFetch from "../useFetch"
+import { useState } from "react";
+import useFetch from "../useFetch";
 
-const Books = ()=>{
-    const { data, loading, error } = useFetch(
-      "https://books-dummy-level-one.vercel.app/books"
-    );
+const Books = () => {
+  const [successMessage, setSuccessMessage] = useState("");
+  const { data, loading, error } = useFetch(
+    "https://books-dummy-level-one.vercel.app/books"
+  );
 
-     if (loading) return <p>Loading...</p>;
+  const handleDelete = async (bookId) => {
+    try {
+      const response = await fetch(
+        `https://books-dummy-level-one.vercel.app/books/${bookId}`,
+        { method: "DELETE" }
+      );
+
+      if (!response.ok) {
+        throw "Failed to delete book.";
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      if (data) {
+        setSuccessMessage("Book deleted Successfully.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setSuccessMessage("Book deleted successfully!");
+      window.location.reload();
+    }
+  };
+
+
+  if (loading) return <p>Loading...</p>;
   if (error) return <p>Error fetching books</p>;
 
   return (
@@ -13,11 +40,17 @@ const Books = ()=>{
       <h2>All Books</h2>
       <ul>
         {data?.map((book) => (
-          <li key={book._id}>{book.title}</li>
+          <li key={book._id}>
+            {book.title}{" "}
+            <button onClick={() => handleDelete(book._id)}>Delete</button>
+          </li>
         ))}
       </ul>
+
+      <p style={{ color: "green" }}>{successMessage}</p>
     </div>
   );
 };
+
 
 export default Books;
